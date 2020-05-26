8898b747ac28ff27415b1c006e24f34e84336d2a
unlikeplatypus <43973727+unlikeplatypus@users.noreply.github.com>
2020-05-26 12:00:36 -0600

    Add files via upload

---
+++ UnitTesting/src/XHeroSpec.js
@@ -0,0 +1,57 @@
+describe("cKey", function() { 
+   
+    it("should Return true",function() { 
+       expect(cKey()).toEqual(true); 
+    }); 
+ 
+ });
+
+ describe("dKey", function() { 
+   
+    it("should Return true",function() { 
+       expect(dKey()).toEqual(true); 
+    }); 
+ 
+ });
+ describe("eKey", function() { 
+   
+    it("should Return true",function() { 
+       expect(eKey()).toEqual(true); 
+    }); 
+ 
+ });
+ describe("fKey", function() { 
+   
+    it("should Return true",function() { 
+       expect(fKey()).toEqual(true); 
+    }); 
+ 
+ });
+ describe("gKey", function() { 
+   
+    it("should Return true",function() { 
+       expect(gKey()).toEqual(true); 
+    }); 
+ 
+ });
+ describe("aKey", function() { 
+   
+    it("should Return true",function() { 
+       expect(aKey()).toEqual(true); 
+    }); 
+ 
+ });
+ describe("bKey", function() { 
+   
+    it("should Return true",function() { 
+       expect(bKey()).toEqual(true); 
+    }); 
+ 
+ });
+ describe("c2Key", function() { 
+   
+    it("should Return true",function() { 
+       expect(c2Key()).toEqual(true); 
+    }); 
+ 
+ });
